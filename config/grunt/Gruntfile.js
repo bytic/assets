e1fb48811b1371c587b81bc656dc566a518cc636
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-12-12 12:01:57 +0200

    add grunt file

---
+++ config/grunt/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function (grunt) {
+    var path = require('path');
+
+    require('load-grunt-config')(grunt, {
+        configPath: [
+            path.join(process.cwd(), 'vendor/42km/common/grunt/config'),
+            path.join(process.cwd(), 'grunt')
+        ],
+        loadGruntTasks: {
+            config: require('./package.json')
+        }
+    });
+
+    // Task definition
+    grunt.registerTask("compileAssets", [
+        "bowercopy", "copy", "concat", "cssmin", "uglify"
+    ]);
+
+    // Task definition
+    grunt.registerTask("default", [
+        "clean", "compileAssets"
+    ]);
+};
