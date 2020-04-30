f5220c3c9c41dd119ee4e8f4a8a211c7eff5d1ec
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-04-30 15:44:32 +0300

    update tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,3 @@ define('PROJECT_BASE_PATH', __DIR__.'/..');
 define('TEST_BASE_PATH', __DIR__);
 define('TEST_FIXTURE_PATH', __DIR__.DIRECTORY_SEPARATOR.'fixtures');
 
-$container = new ByTIC\Container\Container();
-ByTIC\Container\Container::setInstance($container);
