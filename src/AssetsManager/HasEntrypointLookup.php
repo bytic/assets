de299540fadea6f2e259e49ac0ed6778cb2ea915
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-06-22 19:43:34 +0300

    add hasEntrypoint

--- src/AssetsManager/HasEntrypointLookup.php
+++ src/AssetsManager/HasEntrypointLookup.php
@@ -15,6 +15,20 @@ trait HasEntrypointLookup
         return $this->getContainer()->get('assets.entrypoint_lookup')->getEntrypointLookup($entrypointName);
     }
 
+    /**
+     * @param string $entrypointName
+     * @return bool|false
+     */
+    public function hasEntrypoint(string $entrypointName)
+    {
+        try {
+            $this->getEntrypointLookup($entrypointName);
+        } catch (\Exception $e) {
+            return false;
+        }
+        return true;
+    }
+
     public function getWebpackJsFiles(string $entryName, string $entrypointName = '_default'): array
     {
         return $this->getEntrypointLookup($entrypointName)
